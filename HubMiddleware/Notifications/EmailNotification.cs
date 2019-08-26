b9e6272b2cf65cb826eae3b2713380f8699a64f0
DanielRmz <daniel.1912@hotmail.com>
2019-08-26 11:08:21 -0500

    Organized folders

--- HubMiddleware/Notifications/EmailNotification.cs
+++ HubMiddleware/Notifications/EmailNotification.cs
@@ -8,7 +8,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HubMiddleware
+namespace HubMiddleware.Notifications
 {
     public class EmailNotification
     {
