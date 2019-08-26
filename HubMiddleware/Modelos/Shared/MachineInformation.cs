69362940f0facf5d434c22110c4369d2f1c66061
DanielRmz <daniel.1912@hotmail.com>
2019-08-26 16:36:49 -0500

    Added Properties to the watcher classes

--- HubMiddleware/Modelos/Shared/MachineInformation.cs
+++ HubMiddleware/Modelos/Shared/MachineInformation.cs
@@ -3,6 +3,18 @@ namespace HubMiddleware.Modelos.Shared
 {
     public class MachineInformation
     {
+        public string State { get; set; }
 
+        public string TagSignal { get; set; }
+
+        public int Order { get; set; }
+
+        /// <summary>
+        /// No está bien definido el objetivo de esta propiedad.
+        /// </summary>
+        public string Monitoreo { get; set; }
+
+        public string WatcherPath { get; set; }
+        
     }
 }
--- HubMiddleware/Modelos/Shared/Production.cs
+++ HubMiddleware/Modelos/Shared/Production.cs
@@ -1,8 +1,62 @@
-﻿
+﻿using System;
+
 namespace HubMiddleware.Modelos.Shared
 {
     public class Production
     {
+        public double OEE { get; set; }
 
+        public double Performance { get; set; }
+
+        public double Avaibility { get; set; }
+
+        public double Quality { get; set; }
+
+        public int FPY { get; set; }
+
+        public int PPM { get; set; }
+
+        public int LineDownTime { get; set; }
+
+        public int IdMotivo { get; set; }
+
+        public double Produccion { get; set; }
+
+        public double PlannedPieces { get; set; }
+
+        public double Pieces { get; set; }
+
+        public int Scrap { get; set; }
+
+        public int Rework { get; set; }
+
+        public double Rate { get; set; }
+
+        public double ShiftGoal { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime ShiftStart { get; set; }
+
+        public DateTime ShiftEnd { get; set; }
+
+        public int PartId { get; set; }
+
+        /// <summary>
+        /// Esta propiedad se repite con la propiedad de la clase MachineInformation
+        /// </summary>
+        public int ShiftIdOrder { get; set; }
+
+        public int IdTurno { get; set; }
+
+        public string Turno { get; set; }
+
+        public DateTime DownTimeDate { get; set; }
+
+        public int OEEOrder { get; set; }
+
+        public double Tolerance { get; set; }
+
+        public int PieceTime { get; set; }
     }
 }
