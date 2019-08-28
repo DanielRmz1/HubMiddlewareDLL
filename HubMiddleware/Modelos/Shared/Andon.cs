bbcb13a39b05a9a94e49bbf796fc36face5523ee
DanielRmz <daniel.1912@hotmail.com>
2019-08-28 09:38:58 -0500

    Department property created in Andon model

--- HubMiddleware/Modelos/Shared/Andon.cs
+++ HubMiddleware/Modelos/Shared/Andon.cs
@@ -3,6 +3,8 @@ namespace HubMiddleware.Modelos.Shared
 {
     public class Andon
     {
+        public string Department { get; set; }
+
         public string State { get; set; }
 
         public string MachineName { get; set; }
